21a226935b502c6aeb12026885c35f73f0d48131
Andres Rivera Marquez <andres.rivera@iteso.mx>
2021-10-06 21:48:06 -0700

    SPEC, archivo principal decorado y comentado

--- MD5_ARGS.c
+++ MD5_ARGS.c
@@ -1,3 +1,4 @@
+//Inclusion de archivos de encabezado necesarios
 #include <stdio.h>
 #include <stdint.h>
 
@@ -10,7 +11,7 @@
  * Gustavo Alejandro Solorio Ramos.
  * */
 
-//Definicion de las funciones predeterminadas F, G, H e I con un define
+//Definicion de las funciones predeterminadas F, G, H e I con define
 //para hacer la sustitucion mas facil
 #define F(x, y, z) (((x) & (y)) | ((~x) & (z)))
 #define G(x, y, z) (((x) & (z)) | ((y) & (~z)))
@@ -20,26 +21,31 @@
 //Definicion de la funcion de corrimiento izquierdo
 #define CORRIMIENTO_IZQ(num, cantidad) ((num << cantidad) | (num >> (32 - cantidad)))
 
+//Uso de un tipo de variable entera sin signo de 32 bits.
 typedef uint32_t buff32bit;
 
+//Inicio de la funcion main del programa
 int main (int argc, char *argv[]) {
-	/*Se deben crear primero todas las variables y arreglos necesarios
-	 * para el programa, estructuras de datos, arreglos de constantes,
-	 * arreglos para almacenar datos, y las variables temporales donde
-	 * se realizaran las operaciones
-	 * */
+	
+	//Creacion de las registros de 32 bits
 	buff32bit one = 0x80, full = 0xFF, M[16];
-	buff32bit cntBits0=0, cntBits1=0, temp, nBloques=0;
-	buff32bit A,B,C,D,AA,BB,CC,DD, TEMP = 0, H0, H1, H2, H3;
-	unsigned char cicl=0, i = 0, j = 0, k, letra=0; 
-	FILE *Archivo;
-	int l=0, n=0;
-	//Creacion del array de corrimientos S
+	buff32bit cntBits0 = 0, cntBits1 = 0, temp, nBloques = 0; 
+	buff32bit A, B, C, D, AA, BB, CC, DD, H0, H1, H2, H3;
+
+	//Creacion de variables para ciclos
+	unsigned char i = 0, j = 0, k, letra = 0; 
+	int l = 0, n = 0;
+
+	//Creacion de las variables relacionadas al archivo archivo
+	FILE *Archivo; //Puntero al archivo a tratar
+	char *let = argv[1]; //Asignacion de puntero
+	char ruta[100]={"./"}; //Array donde se guarda la ruta del archivo
+
+	//Creacion de los arreglos de constantes
 	unsigned char S[4][4] = {{7, 12, 17, 22}, 
 				{5,  9, 14, 20},
 				{4, 11, 16, 23},
 				{6, 10, 15, 21}};
-	//Creacion del array de K
 	buff32bit K[] = {
         0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
         0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
@@ -58,171 +64,144 @@ int main (int argc, char *argv[]) {
         0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
         0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 
-	 /*------------Lectura del archivo------------*/
-	char *let = argv[1]; //Asignacion de puntero
-	char ruta[50]={"./"}; //Array donde se guarda la ruta del archivo
+	/*------------Inicio del programa.------------*/
 
-	//Organizacion de la ruta relativa
+	//Lectura del archivo
 	i = 2;
 	while (*let != '\0'){
 		ruta[i] = *let;
 		i++;
 		let++;
 	}
+	//Se aniade el fin de cadena
 	ruta[i] = '\0';
 	Archivo = fopen(ruta, "r");
-	/*
-	//Se imprime la ruta que se obtiene
-	printf("La ruta queda: %s\n", ruta);
-	*/
 
-	//Se asignan los valores iniciales al buffer en little endian
-	A = 0x67452301;
-	B = 0xEFCDAB89;
-	C = 0x98BADCFE;
-	D = 0x10325476;
-	//Se inicializan a los mismos valores las variables que contendran
-	//el hash MD5 calculado final.
-	H0 = A;
-	H1 = B;
-	H2 = C;
-	H3 = D;
+	//Inicializacion de los valores iniciales del registro HASH MD5
+	H0 = 0x67452301;
+	H1 = 0xEFCDAB89;
+	H2 = 0x98BADCFE;
+	H3 = 0x10325476;
 
 
 //Ciclo for que repite el proceso de obtencion de hash dependiendo del numero
 //de bloques que se obtuvieron
 for(k = 0; k <= nBloques; k++){
-	/*
-	 * Este bloque de codigo se encarga de leer el archivo y pasarlo a un
-	 * array de registros de 32 bits llamado M para cada bloque.
-	 */
-	if (k==0){
-		A = 0x67452301;
-		B = 0xEFCDAB89;
-		C = 0x98BADCFE;
-		D = 0x10325476;
-	}
-	else{
-		A = H0;
-		B = H1;
-		C = H2;
-		D = H3;
-	}
+	
+	//Los registros del MD5 se asignan a los buffer A, B, C y D
+	A = H0;
+	B = H1;
+	C = H2;
+	D = H3;
+
 	//Se setea el arreglo M a 0 cada inicio del ciclo
 	for (i=0; i < 16; i++) M[i] = 0;
 
-	//Paso de informacion del archivo al bloque M
+	//Ciclo for que recorre las 16 posiciones de los bloques M
 	for (i = 0; i < 16 && letra != 0XFF; i++){
-			for (j = 0; j < 4; j++){
+		
+		//Ciclo for para recorrer los 4 bytes de las words de M
+		for (j = 0; j < 4; j++){
+
+			//Se lee el contenido del archivo caracter por caracter
+			letra = fgetc(Archivo);
+
+			//Mientras no se detecte el fin del archivo
+			if (letra != 0xFF){
+				
 				//Append de los caracteres del archivo a las
 				//words de M, en little endian
-				letra = fgetc(Archivo);
-				if (letra != 0xFF){
-					M[i] = M[i] | ((buff32bit)(letra)<<(8*j));
-					
-					//Condicional que contara el tamaño
-					//del mensaje
-					if (cntBits0 == 0xFFFFFFF8){
-						cntBits0 += 8;
-						cntBits1 += 1;
-					}
-					else cntBits0 += 8;
+				M[i] = M[i] | ((buff32bit)(letra)<<(8*j));	
+				
+				//Condicional que contara el tamaño
+				//del mensaje
+				if (cntBits0 == 0xFFFFFFF8){
+					cntBits0 = 0;
+					cntBits1 += 1;
 				}
-				else {
-					M[i] = M[i]|(one<<(8*j));
-					break;
-				}
+				else cntBits0 += 8;
 			}
-			if (letra == 0XFF) break;
+			//Si detecta un fin de archivo inyecta el 0x80
+			else {
+				M[i] = M[i]|(one<<(8*j));
+				break;
+			}
+		}
+		//Si se detecto el fin de archivo se sale del ciclo
+		if (letra == 0XFF) break;
 	}
 	
 	//Si el bloque de codigo anterior abarca mas de 448 bits entonces
 	//aumenta el contador de bloques M en uno
 	if (i >= 14) nBloques++;
-	//Ingreso del tamaño del mensaje
+
+	//Si se detecto el fin de archivo, y se tienen las ultimas dos lineas
+	//disponibles para inyectarle el tamaño del archivo
 	if ((letra == 0XFF) && (i < 14)){
-		//Si las condiciones se cumplen, se agregan en las ultimas dos
-		//palabras el tamaño del mensaje.
+
+		//Ciclo for para recorrer las ultimas 2 words del bloque
 		for (j = 0; j < 4; j++){
+			
+			//Ecuaciones para guardar el tamaño del archivo en el
+			//bloque M
 			temp = (cntBits0 >> (8*j)) & full;
 			M[14] = M[14] | (temp << (8*j));
 			
-			temp =((cntBits1 >> (8*j)) & full); 
+			temp = (cntBits1 >> (8*j)) & full; 
 			M[15] = M[15] | (temp << (8*j));
 		}
 	} 
-	//Estas lineas de codigo sirven para desplegar los bloques M de cada
-	//iteracion.
-	//printf("----------Impresion del bloque M%d----------\n", k+1);
-	//for (cicl=0; cicl < 16; cicl++) printf("0x%X\n", M[cicl]);
 
-/*printf("------------Inicio del algoritmo----------------\n");
-printf("Valores de (A B C D)\n");
-printf("t%d %X %X %X %X\n", n, A,B,C,D);*/
-//printf("size of: %d %d %d %d\n", sizeof(A), sizeof(B), sizeof(C), sizeof(D));
-for (i=0;i<4;i++){
+//Ciclo for utilizado para realizar las 4 vueltas de las operaciones F,G,H y I
+for (n = 0, i = 0; i < 4; i++){
 	
-	for (j=0; j<16; j++, n++){
+	//Ciclo for que realizara las 16 vueltas para cada funcion.
+	for (j = 0; j < 16; j++, n++){
+
+		//Condicional if que calcula la palabra a utilizar para cada
+		//ciclo
 		if (n < 16) l = n;
 		else if (n < 32) l = (5*n + 1) % 16;
 		else if (n < 48) l = (3*n + 5) % 16;          
 		else l= (7*n) % 16;
 
-		//printf("M[%d]=%lX   K[%d]=%lX   S[%d][%d]=%d\n",l,M[l], n, K[n], i, j%4, S[i][j%4]);
-		if (i == 0 ){
-			AA = D;
-			BB = (B + CORRIMIENTO_IZQ(((A +F(B,C,D) + M[l] + K [n])),S[i][j%4]));
-			CC = B;
-		        DD = C;
+		//Se guardan en registros auxiliares los valores de A,B,C,D
+		AA = D;
+		CC = B;
+		DD = C;
+		//Condicional if que determina la funcion que se utilizara
+		//en cada vuelta para calcular la nueva B
+		if (i == 0 ) BB = B + CORRIMIENTO_IZQ(((A +F(B,C,D) + M[l] + K [n])),S[i][j%4]);
+		else if (i == 1) BB = B + CORRIMIENTO_IZQ(((A +G(B,C,D) + M[l] + K [n])),S[i][j%4]);
+		else if (i == 2) BB = B + CORRIMIENTO_IZQ(((A +H(B,C,D) + M[l] + K [n])),S[i][j%4]);
+		else if (i == 3) BB = B + CORRIMIENTO_IZQ(((A +I(B,C,D) + M[l] + K [n])),S[i][j%4]);
 
-		}
-		else if (i == 1){
-			AA = D;
-			BB = B + CORRIMIENTO_IZQ(((A +G(B,C,D) + M[l] + K [n])),S[i][j%4]);
-			CC = B;
-		        DD = C;
-		}
-		else if (i == 2){
-			AA = D;
-			BB = B + CORRIMIENTO_IZQ(((A +H(B,C,D) + M[l] + K [n])),S[i][j%4]);
-			CC = B;
-		        DD = C;
-		}
-		else if (i == 3){
-			AA = D;
-			BB = B + CORRIMIENTO_IZQ(((A +I(B,C,D) + M[l] + K [n])),S[i][j%4]);
-			CC = B;
-		        DD = C;
-		}
+		//Una vez reacomodadas en las variables auxiliares se asignan
+		//a los registros originales
 		A = AA;
 		B = BB;
 		C = CC;
 		D = DD;
-		//printf("t%d %X %X %X %X\n", n+1, A,B,C,D);	
 	}
 }
-
-	//printf("----------Impresion del bloque M%d----------\n", k+1);
-	//for (cicl=0; cicl < 16; cicl++) printf("0x%8lx\n", M[cicl]);
-	//fin de las 64 iteraciones para cada bloque
-	//Se actualiza el valor del hash global.
-	//printf("%X %X %X %X\n", A,B,C,D);
-	//printf("%X %X %X %X\n", H0,H1,H2,H3);
-	//printf("-------------------------------------------\n");
+	//Una vez terminadas las 64 iteraciones del bloque M actual se le suman
+	//los valores calculados de A,B,C y D
 	H0+=A;
 	H1+=B;
 	H2+=C;
 	H3+=D;
-	//printf("%X %X %X %X\n", H0,H1,H2,H3);
-	
-	n = 0;
 }
+
+//Una vez terminado el codigo, se cierra el archivo.
+fclose(Archivo);
+
 //Fin del algoritmo, inversion del hash a little endian.
 A=0;
 B=0;
 C=0;
 D=0;
 
+//Ciclo for para convertir a Big-endian el HASH MD5
 for (i=0; i<4; i++){
 	A = A | ((H0>>(i*8)) & full)<<(8*(3-i));
 	B = B | ((H1>>(i*8)) & full)<<(8*(3-i));
@@ -230,8 +209,10 @@ for (i=0; i<4; i++){
 	D = D | ((H3>>(i*8)) & full)<<(8*(3-i));
 }
 
-printf("MD5 = %X %X %X %X\n", A, B, C, D);
+//Desplegar el valor del hash semejante al md5sum de linux
+printf("%x%x%x%x  %s\n", A, B, C, D, argv[1]);
 
+//Fin del programa
        	return 0; 
 } 
 /*
